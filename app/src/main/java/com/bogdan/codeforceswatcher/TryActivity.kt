d30c680bdf345f19ff62dca0efa8359cace3d98f
evtushenko-bogdan <evtushenko-bogdan>
2018-03-12 12:07:24 +0200

    #4 Add UI

--- app/src/main/java/com/bogdan/codeforceswatcher/TryActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/TryActivity.kt
@@ -15,10 +15,6 @@ class TryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_try)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
