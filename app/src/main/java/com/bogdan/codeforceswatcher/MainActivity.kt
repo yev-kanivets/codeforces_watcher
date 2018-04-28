5dc3ca73cc653029563f0501e526dccabb6d73c2
evtushenko-bogdan <evtushenko-bogdan>
2018-04-28 18:18:46 +0300

    #19.Save added users in SPref.

--- app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bogdan.codeforceswatcher
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,16 +8,21 @@ import android.view.View
 import android.view.View.OnClickListener
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.ArrayAdapter
-
+import android.R.id.edit
+import android.util.Log
+import android.R.id.edit
 
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     val names = mutableListOf<String>()
+    var SAVED_TEXT = "Text"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadText()
 
         btnShow.setOnClickListener(this)
 
@@ -30,6 +36,29 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         val HANDLES = "Handle"
     }
 
+    fun loadText() {
+        var s = ""
+        val sPref = getPreferences(Context.MODE_PRIVATE)
+        val savedText = sPref.getString(SAVED_TEXT, "")
+        for (i in savedText) {
+            if (i != ' ') {
+                s += i
+            } else {
+                names.add(s)
+                s = ""
+            }
+        }
+        if (s != "")
+            names.add(s)
+    }
+
+    fun saveText() {
+        val sPref = getPreferences(Context.MODE_PRIVATE)
+        val ed = sPref.edit()
+        ed.putString(SAVED_TEXT, sPref.getString(SAVED_TEXT, "") + " " + etHandle.text.toString())
+        ed.commit()
+    }
+
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btnShow -> {
@@ -37,7 +66,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 intent.putExtra("Handle", etHandle.text.toString())
                 startActivity(intent)
                 names.add(etHandle.text.toString())
-
+                saveText()
             }
             else -> {
             }
