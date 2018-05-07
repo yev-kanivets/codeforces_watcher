8efab33870e7b231eb37791ba90dc52714dbb7da
evtushenko-bogdan <evtushenko-bogdan>
2018-05-07 11:36:19 +0300

    #24.Add only real handles to the list.

--- app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
@@ -14,6 +14,12 @@ import android.R.id.edit
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity(), OnClickListener {
@@ -72,16 +78,49 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         ed.commit()
     }
 
+    private fun loadUser() {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://www.codeforces.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val userApi = retrofit.create(UserApi::class.java)
+
+        val user = userApi.user(etHandle.text.toString())
+
+        user.enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful) {
+                    names.add(0, etHandle.text.toString())
+                    Log.d("MY_TAG", etHandle.text.toString())
+                    adapter.notifyDataSetChanged()
+                    saveText()
+                    etHandle.text = null
+                } else {
+                    showError()
+                    etHandle.text = null
+                }
+            }
+
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                showError()
+                etHandle.text = null
+            }
+        })
+    }
+
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btnShow -> {
-                names.add(0, etHandle.text.toString())
-                adapter.notifyDataSetChanged()
-                saveText()
-                etHandle.text = null
+                loadUser()
             }
             else -> {
             }
         }
     }
+
+    fun showError() {
+        Toast.makeText(applicationContext, "Wrong handle!", Toast.LENGTH_SHORT).show()
+    }
+
 }
