b1452e5cd70d8095206cd2ad1af6b924d95e9430
evtushenko-bogdan <evtushenko-bogdan>
2018-06-01 13:16:47 +0300

    #41. Color handle and rating in ListView.

--- app/src/main/java/com/bogdan/codeforceswatcher/UserAdapter.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/UserAdapter.kt
@@ -1,13 +1,16 @@
 package com.bogdan.codeforceswatcher
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import com.bogdan.codeforceswatcher.R.color.*
 
-class UserAdapter internal constructor(ctx: Context, private var objects: MutableList<User>) : BaseAdapter() {
+@Suppress("DEPRECATION")
+class UserAdapter internal constructor(private val ctx: Context, private var objects: MutableList<User>) : BaseAdapter() {
 
     private var lInflater: LayoutInflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
@@ -30,13 +33,34 @@ class UserAdapter internal constructor(ctx: Context, private var objects: Mutabl
         }
 
         val p = getItem(position)
+
         (view!!.findViewById<View>(R.id.tv1) as TextView).text = p.handle
         if (p.rating == null) {
             (view.findViewById<View>(R.id.tv2) as TextView).text = ""
         } else
             (view.findViewById<View>(R.id.tv2) as TextView).text = p.rating.toString()
+        Log.d("TAG",p.rank + "vs" + getColor(p.rank!!).toString())
+
+        (view.findViewById<View>(R.id.tv1) as TextView).setTextColor(ctx.resources.getColor(getColor(p.rank)))
+        (view.findViewById<View>(R.id.tv2) as TextView).setTextColor(ctx.resources.getColor(getColor(p.rank)))
 
         return view
+    }
+
+    private fun getColor(rank:String):Int{
+        return when(rank){
+            "newbie" -> grey
+            "pupil" -> green
+            "specialist" -> bluegreen
+            "expert" -> blue
+            "master candidate" -> purple
+            "master" -> orange
+            "international master" -> orange
+            "grandmaster" -> red
+            "international grandmaster" -> red
+            "legendary grandmaster" -> red
+            else -> 1
+        }
     }
 
 }
