77e49b2ac7d21c4827f57b704864fe209ea50a84
evtushenko-bogdan <evtushenko-bogdan>
2018-06-12 15:59:54 +0300

    #64. Add fixes review.

--- app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/MainActivity.kt
@@ -21,7 +21,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
 
     private val users = mutableListOf<User>()
-    var it: List<User>? = null
+    lateinit var it: List<User>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
         lvMain.onItemClickListener = OnItemClickListener { _, view, _, id ->
             val intent = Intent(this, TryActivity::class.java)
             intent.putExtra("Handle", (view.findViewById<TextView>(R.id.tv1) as TextView).text)
-            intent.putExtra("Id", it!![it!!.size - 1 - id.toInt()].id.toString())
+            intent.putExtra("Id", it[it.size - 1 - id.toInt()].id.toString())
             startActivity(intent)
         }
 
@@ -51,8 +51,8 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
 
         liveData.observe(this, Observer<List<User>> { t ->
             users.clear()
-            it = t
-            for (element in t!!.size - 1 downTo 0) {
+            it = t!!
+            for (element in t.size - 1 downTo 0) {
                 users.add(t[element])
                 userAdapter.notifyDataSetChanged()
             }
@@ -75,22 +75,22 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
 
         val userApi = retrofit.create(UserApi::class.java)
 
-        val user = userApi.user(handle)
+        val userCall = userApi.user(handle)
 
-        user.enqueue(object : Callback<UserResponse> {
+        userCall.enqueue(object : Callback<UserResponse> {
             override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
                 for ((counter, element) in response.body()!!.result.withIndex()) {
-                    val rating = userApi.rating(element.handle)
-                    element.id = it!![counter].id
-                    if (element.rating == it!![counter].rating) {
-                        element.ratingChanges = it!![counter].ratingChanges
+                    val ratingCall = userApi.rating(element.handle)
+                    element.id = it[counter].id
+                    if (element.rating == it[counter].rating) {
+                        element.ratingChanges = it[counter].ratingChanges
                         userDao.update(element)
                     } else {
-                        rating.enqueue(object : Callback<RatingChangeResponse> {
+                        ratingCall.enqueue(object : Callback<RatingChangeResponse> {
                             override fun onResponse(call: Call<RatingChangeResponse>, response: Response<RatingChangeResponse>) {
                                 if (response.isSuccessful) {
                                     element.ratingChanges = response.body()!!.result
-                                    MainActivity.userDao.update(element)
+                                    userDao.update(element)
                                 }
                             }
 
@@ -108,7 +108,7 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
 
     override fun onRefresh() {
         var handles = ""
-        for (element in this.it!!) {
+        for (element in this.it) {
             handles += element.handle + ";"
         }
         loadUser(handles)
