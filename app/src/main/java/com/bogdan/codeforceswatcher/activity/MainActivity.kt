e63586a54766c0aee911b4a9debbb21d06f3d10c
evtushenko-bogdan <evtushenko-bogdan>
2018-08-31 15:43:02 +0300

    #119. Add tabs to MainActivity

--- app/src/main/java/com/bogdan/codeforceswatcher/activity/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/activity/MainActivity.kt
@@ -2,37 +2,29 @@ package com.bogdan.codeforceswatcher.activity
 
 import android.app.AlarmManager
 import android.app.PendingIntent
-import android.arch.lifecycle.Observer
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
-import android.support.annotation.RequiresApi
-import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import com.bogdan.codeforceswatcher.CwApp
 import com.bogdan.codeforceswatcher.R
-import com.bogdan.codeforceswatcher.adapter.UserAdapter
-import com.bogdan.codeforceswatcher.model.User
+import com.bogdan.codeforceswatcher.fragment.ContestsFragment
+import com.bogdan.codeforceswatcher.fragment.UsersFragment
 import com.bogdan.codeforceswatcher.receiver.RatingUpdateReceiver
 import com.bogdan.codeforceswatcher.util.Prefs
-import com.bogdan.codeforceswatcher.util.UserLoader
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
-class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var userAdapter: UserAdapter
     private var counterIcon: Int = 0
     private val prefs = Prefs(this)
-    private lateinit var spinnerAdapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,48 +43,35 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
     }
 
     private fun initViews() {
-        fab.setOnClickListener(this)
-        swiperefresh.setOnRefreshListener(this)
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(UsersFragment(), "Users")
+        adapter.addFragment(ContestsFragment(), "Contests")
+        viewPager.adapter = adapter
 
-        spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-                resources.getStringArray(R.array.array_sort))
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
 
-        spSort.adapter = spinnerAdapter
-
-        spSort.setSelection(counterIcon)
-
-        spSort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                counterIcon = position
-                updateList(CwApp.app.userDao.getAll())
-                prefs.writeCounter(counterIcon)
+            override fun onPageScrollStateChanged(state: Int) {
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>) {
-
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
             }
-        }
 
-        userAdapter = UserAdapter(listOf(), this)
-
-        rvMain.adapter = userAdapter
-        rvMain.layoutManager = LinearLayoutManager(this)
-        rvMain.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (dy > 0 && fab.visibility == View.VISIBLE) {
-                    fab.hide()
-                } else if (dy < 0 && fab.visibility != View.VISIBLE) {
-                    fab.show()
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        fab.show()
+                        spSort.visibility = View.VISIBLE
+                    }
+                    1 -> {
+                        fab.hide()
+                        spSort.visibility = View.INVISIBLE
+                    }
                 }
             }
+
         })
 
-        val liveData = CwApp.app.userDao.getAllLive()
-        liveData.observe(this, Observer<List<User>> { userList ->
-            userList?.let { updateList(it) }
-        })
+        tabs.setupWithViewPager(viewPager)
     }
 
     private fun startAlarm() {
@@ -107,28 +86,25 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener, 
         prefs.writeAlarm(alarmManager.toString())
     }
 
-    private fun updateList(users: List<User>) {
-        when (counterIcon) {
-            0 -> userAdapter.setItems(users.reversed())
-            1 -> userAdapter.setItems(users.sortedByDescending(User::rating))
-            2 -> userAdapter.setItems(users.sortedBy(User::rating))
-            3 -> userAdapter.setItems(users.sortedByDescending { user -> user.ratingChanges.lastOrNull()?.ratingUpdateTimeSeconds })
-            4 -> userAdapter.setItems(users.sortedBy { user -> user.ratingChanges.lastOrNull()?.ratingUpdateTimeSeconds })
+    class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
         }
-    }
 
-    override fun onRefresh() {
-        UserLoader.loadUsers(CwApp.app.userDao.getAll()) { runOnUiThread { swiperefresh.isRefreshing = false } }
-    }
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
 
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.fab -> {
-                startActivity(Intent(this, AddUserActivity::class.java))
-            }
-            else -> {
-            }
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return mFragmentTitleList[position]
         }
     }
 
---
+++ app/src/main/java/com/bogdan/codeforceswatcher/fragment/UsersFragment.kt
@@ -0,0 +1,126 @@
+package com.bogdan.codeforceswatcher.fragment
+
+import android.arch.lifecycle.Observer
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.AppCompatSpinner
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.bogdan.codeforceswatcher.CwApp
+import com.bogdan.codeforceswatcher.R
+import com.bogdan.codeforceswatcher.activity.AddUserActivity
+import com.bogdan.codeforceswatcher.adapter.UserAdapter
+import com.bogdan.codeforceswatcher.model.User
+import com.bogdan.codeforceswatcher.util.Prefs
+import com.bogdan.codeforceswatcher.util.UserLoader
+import kotlinx.android.synthetic.main.fragment_users.*
+
+class UsersFragment : android.support.v4.app.Fragment(), SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
+
+    private lateinit var userAdapter: UserAdapter
+    private var counterIcon: Int = 0
+
+    override fun onRefresh() {
+        UserLoader.loadUsers(CwApp.app.userDao.getAll()) { activity?.runOnUiThread { swiperefresh.isRefreshing = false } }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.let {
+            val prefs = Prefs(it)
+            counterIcon = if (prefs.readCounter().isEmpty()) 0 else prefs.readCounter().toInt()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_users, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        val fab = activity?.findViewById<FloatingActionButton>(R.id.fab)
+        val spSort = activity?.findViewById<AppCompatSpinner>(R.id.spSort)
+        fab?.visibility = View.VISIBLE
+        spSort?.visibility = View.VISIBLE
+        fab?.setOnClickListener(this)
+        swiperefresh.setOnRefreshListener(this)
+
+        val spinnerAdapter = ArrayAdapter(activity, android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.array_sort))
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spSort?.adapter = spinnerAdapter
+
+        spSort?.setSelection(counterIcon)
+
+        spSort?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                counterIcon = position
+                updateList(CwApp.app.userDao.getAll())
+                activity?.let {
+                    val prefs = Prefs(it)
+                    prefs.writeCounter(counterIcon)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+        activity?.let {
+            userAdapter = UserAdapter(listOf(), it)
+            rvMain.adapter = userAdapter
+            rvMain.layoutManager = LinearLayoutManager(it)
+            rvMain.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (dy > 0 && fab?.visibility == View.VISIBLE) {
+                        fab.hide()
+                    } else if (dy < 0 && fab?.visibility != View.VISIBLE) {
+                        fab?.show()
+                    }
+                }
+            })
+
+            val liveData = CwApp.app.userDao.getAllLive()
+            liveData.observe(this, Observer<List<User>> { userList ->
+                userList?.let { usersList -> updateList(usersList) }
+            })
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.fab -> {
+                startActivity(Intent(activity, AddUserActivity::class.java))
+            }
+            else -> {
+            }
+        }
+    }
+
+    private fun updateList(users: List<User>) {
+        when (counterIcon) {
+            0 -> userAdapter.setItems(users.reversed())
+            1 -> userAdapter.setItems(users.sortedByDescending(User::rating))
+            2 -> userAdapter.setItems(users.sortedBy(User::rating))
+            3 -> userAdapter.setItems(users.sortedByDescending { user -> user.ratingChanges.lastOrNull()?.ratingUpdateTimeSeconds })
+            4 -> userAdapter.setItems(users.sortedBy { user -> user.ratingChanges.lastOrNull()?.ratingUpdateTimeSeconds })
+        }
+    }
+
+}
