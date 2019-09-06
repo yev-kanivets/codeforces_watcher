1e9e1fdd4dd516a440b7fb388b598feba750f782
Bogdan Yevtushenko <evtushencko.b@gmail.com>
2019-09-06 15:11:28 +0300

    #153. Change date format in Contest card.

--- app/src/main/java/com/bogdan/codeforceswatcher/adapter/ContestAdapter.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/adapter/ContestAdapter.kt
@@ -28,7 +28,7 @@ class ContestAdapter(private var items: List<Contest>, private val ctx: Context)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val contest = items[position]
         holder.tvContestName.text = contest.name
-        holder.tvContestTime.text = getDateTime(contest.time)
+        holder.tvContestTime.text = getDateFromSeconds(contest.time)
         holder.ivAddToCalendar.setOnClickListener { addContestToCalendar(items[position]) }
     }
 
@@ -48,8 +48,8 @@ class ContestAdapter(private var items: List<Contest>, private val ctx: Context)
         notifyDataSetChanged()
     }
 
-    private fun getDateTime(seconds: Long): String {
-        return SimpleDateFormat("kk:mm MMM d, yyyy", Locale.ENGLISH).format(Date(seconds * 1000)).toString()
+    private fun getDateFromSeconds(seconds: Long): String {
+        return SimpleDateFormat("kk:mm MMM d, EEEE", Locale.ENGLISH).format(Date(seconds * 1000)).toString()
     }
 
     private fun getCalendarTime(time: Long): String {
