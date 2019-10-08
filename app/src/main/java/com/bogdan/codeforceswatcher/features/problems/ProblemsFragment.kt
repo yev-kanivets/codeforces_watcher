ce5409488540d2afd7b2db518f7fdb7a3034c411
Bohdan Yevtushenko <evtushencko.b@gmail.com>
2019-10-08 17:51:44 +0300

    #8. Implement problems tab stub.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragment.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragment.kt
@@ -1,7 +1,17 @@
 package com.bogdan.codeforceswatcher.features.problems
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bogdan.codeforceswatcher.R
 
 class ProblemsFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_problems, container, false)
 }
