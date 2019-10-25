bf1a658590324db8b430040123787da9b68ee2da
Bohdan Yevtushenko <evtushencko.b@gmail.com>
2019-10-25 11:54:22 +0300

    #17. Implement 'Share Codeforces Watcher' dialog.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bogdan.codeforceswatcher.features
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.PorterDuff
 import android.net.Uri
@@ -120,8 +121,25 @@ class MainActivity : AppCompatActivity(), StoreSubscriber<UIState> {
 
     private fun onActionsTabSelected() {
         llSorting.visibility = View.GONE
-        fab.setOnClickListener(null)
+        fab.setOnClickListener { showShareDialog() }
         fab.setImageDrawable(getDrawable(R.drawable.ic_share))
+    }
+
+    private fun showShareDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.share_cw))
+            .setMessage(getString(R.string.help_cw_make_more_social))
+            .setCancelable(false)
+            .setPositiveButton(getString(R.string.share)) { _, _ -> share() }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .create()
+            .show()
+    }
+
+    private fun share() = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, getString(R.string.share_cw_message))
+        startActivity(this)
     }
 
     private fun onProblemsTabSelected() {
