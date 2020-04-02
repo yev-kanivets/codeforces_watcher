d788ebbec8f7be1a78c42bbbbed87dad0daf1d0c
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2020-04-02 14:41:46 +0300

    Lazy init for CWDatabase.

--- common/src/iOSMain/kotlin/io/xorum/codeforceswatcher/database/Database.kt
+++ common/src/iOSMain/kotlin/io/xorum/codeforceswatcher/database/Database.kt
@@ -4,9 +4,6 @@ import io.xorum.codeforceswatcher.CWDatabase
 import io.xorum.codeforceswatcher.redux.sqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-/**
- * https://github.com/cashapp/sqldelight/blob/d0fcd05115cd6d8cdd7ed91901b1a7676a16a637/sample/common/src/iosMain/kotlin/com/example/sqldelight/hockey/data/Db.kt
- */
 fun initDatabase() {
-    sqlDriver = NativeSqliteDriver(CWDatabase.Schema, "database.db")
+    sqlDriver = NativeSqliteDriver(CWDatabase.Schema, "database")
 }
